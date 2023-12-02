8e4202d9d14b1c59ddfa46177abfa09b0e7a2dee
Joel Uzcategui <joel@tianguis-mx.com>
2023-12-01 22:10:26 -0400

    fix ProductItem hover scale effect

--- src/lib/components/app/productItem.svelte
+++ src/lib/components/app/productItem.svelte
@@ -9,9 +9,11 @@
     export let id = 1;
 </script>
 
-<div class="border-2 rounded-lg bg-white border-gray-500 hover:border-gray-900 transition-all overflow-hidden">
+<div class="group border-2 rounded-lg bg-white border-gray-500 hover:border-gray-900 transition-all overflow-hidden">
     <div class="relative w-full aspect-w-1 aspect-h-1">
-        <img src={images[0]} alt={name} class="object-cover w-full h-full hover:scale-110 transition-all" />
+        <div class="overflow-hidden w-full h-full">
+            <img src={images[0]} alt={name} class="object-cover w-full h-full group-hover:scale-110 transition-all" />
+        </div>
     </div>
     <div class="flex flex-col items-center justify-center rounded-lg p-4">
         <div class="flex flex-row items-center justify-between w-full">
